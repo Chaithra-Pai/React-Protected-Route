667966ab79fc7a5719fd9b6541239474c7398374
Chaithra <chaithrapai@gmail.com>
2022-08-17 18:40:49 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import { Login } from './pages/Login';
+import PrivateRoutes from './utils/PrivateRoutes';
+import { Home } from './pages/Home';
+import { Products } from './pages/Products';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Protected Router</h1> 
+      <Routes>
+        <Route path='/login' element={<Login/>} />
+        <Route element={<PrivateRoutes/>} >
+          <Route path='/' element={<Home/>} exact/>
+          <Route path='/product' element={<Products/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
